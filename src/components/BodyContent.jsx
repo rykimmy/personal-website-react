c41892128be4f0a3f0490c777ca1f078ff072a8b
Ryan <rkim20@choate.edu>
2023-05-10 09:27:47 -0400

    added navbar slide animation; now starting animations for sections

--- src/components/BodyContent.jsx
+++ src/components/BodyContent.jsx
@@ -3,15 +3,32 @@ import Education from './Education/Education';
 import Experience from './Experience/Experience';
 import Projects from './Projects/Projects';
 import ActivitiesInterests from './ActivitiesInterests/ActivitiesInterests';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 function BodyContent() {
     return (
         <div>
-            <About />
+            <AnimationOnScroll animateIn="animate__bounceIn">
+                <About />
+            </AnimationOnScroll>
+            <AnimationOnScroll animateIn="animate__bounceIn">
+                <Education />
+            </AnimationOnScroll>
+            <AnimationOnScroll animateIn="animate__bounceIn">
+                <Experience />
+            </AnimationOnScroll>
+            <AnimationOnScroll animateIn="animate__bounceIn">
+                <Projects />
+            </AnimationOnScroll>
+            <AnimationOnScroll animateIn="animate__bounceIn">
+                <ActivitiesInterests />
+            </AnimationOnScroll>
+
+            {/* <About />
             <Education />
             <Experience />
             <Projects />
-            <ActivitiesInterests />
+            <ActivitiesInterests /> */}
         </div>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,32 @@
+import { Link } from "react-scroll";
+
 function NavBar() {
     return (
         <div>
             <header>
                 <h1 id="title">Ryan Kim</h1>
                 <nav>
-                    <p><a className="navbar" href="#about">About</a></p>
+                    {/* <p><a className="navbar" href="#about">About</a></p>
                     <p><a className="navbar" href="#education">Education</a></p>
                     <p><a className="navbar" href="#experience">Experience</a></p>
                     <p><a className="navbar" href="#projects">Projects</a></p>
-                    <p><a className="navbar" href="#activitiesandinterests">Activities and Interests</a></p>{'  '}
+                    <p><a className="navbar" href="#activitiesandinterests">Activities and Interests</a></p>{'  '} */}
+
+                    <Link activeClass="active" smooth={true} spy="navbar" to="about">
+                        <p><a className="navbar" href="#about">About</a></p>
+                    </Link>
+                    <Link activeClass="active" smooth={true} spy="navbar" to="education">
+                        <p><a className="navbar" href="#education">Education</a></p>
+                    </Link>
+                    <Link activeClass="active" smooth={true} spy="navbar" to="experience">
+                        <p><a className="navbar" href="#experience">Experience</a></p>
+                    </Link>
+                    <Link activeClass="active" smooth={true} spy="navbar" to="projects">
+                        <p><a className="navbar" href="#projects">Projects</a></p>
+                    </Link>
+                    <Link activeClass="active" smooth={true} spy="navbar" to="activitiesandinterests">
+                        <p><a className="navbar" href="#activitiesandinterests">Activities and Interests</a></p>{'  '}
+                    </Link>
                 </nav>
             </header>
         </div>
