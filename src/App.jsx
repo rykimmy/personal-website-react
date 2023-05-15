abb15ae6cd4d7fb291a76e345a9cb17b4c269ecb
Ryan <rkim20@choate.edu>
2023-05-15 19:44:03 -0400

    touched up some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { useState } from 'react'
 import NavBar from './components/NavBar';
 import ProfilePic from './components/ProfilePic';
 import BodyContent from './components/BodyContent';
 import Contacts from './components/Contacts/Contacts';
-// import './App.css'
-import './styles.css'
+// import './App.css';
+import './styles.css';
 
 // Upon refresh, goes to top of page
 window.onbeforeunload = function () {
